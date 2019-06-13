2586f751916f1d01837bed3090228acb19e03443
Roko <rokokovac98@gmail.com>
2019-06-13 13:11:36 +0200

    Add validation, refactor

--- scraper.py
+++ scraper.py
@@ -5,22 +5,44 @@ rss_url = "https://www.index.hr/rss/vijesti"
 postList = []
 posts = feedparser.parse(rss_url).entries
 
-for post in posts:
-    title = post['title']
+def word_valid(word):
+    if (len(word) < 4):
+        return False
+    if (word == "FOTO"):
+        return False
+    if (word == "VIDEO"):
+        return False
 
-    summary = post['summary']
-    match = re.search("(?<=src=\").*?(?=\")", summary)   
+    return True
+
+def get_keywords(title):
+    words = title.split()
+    clean = []
+
+    for word in words:
+        match = re.search('^(?:[^\\W\\d_]|)+$', word)
+        match_string = ""
+        if (match != None):
+            match_string += match.string
+
+        word = match_string.upper()
+        
+        if word_valid(word):
+            clean.append(word)
+    
+    return ','.join(clean)
+
+for post in posts:
+    match = re.search("(?<=src=\").*?(?=\")", post['summary'])   
     beginning = match.span()[0]
     end = match.span()[1]
 
-    image = match.string[beginning:end]
-    text = match.string[end+4:]    
-
     postList.append({
-        "title":title,
-        "text":text,
-        "image":image
-        })
+        "title":post['title'],
+        "text":match.string[end+4:],
+        "image":match.string[beginning:end],
+        "keywords":get_keywords(post['title'])
+        }) 
 
 def get_posts():
     return postList
